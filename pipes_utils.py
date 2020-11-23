512a9a993ef5349e1ea1ebb391769a194c9dd96b
aswheaton <alex.s.wheaton@gmail.com>
2020-11-23 11:24:22 +0000

    Added (de)reddening utilities, noisify, and fixed headers.

--- pipes_utils.py
+++ pipes_utils.py
@@ -2,26 +2,24 @@ import bagpipes as pipes
 import numpy as np
 import matplotlib.pyplot as plt
 
-def noisify_1(value, percent_error):
-    """
-    Receives a value and desired percent error on that value. Draws a number
-    from a Gaussian distribution around that value with width equal to twice
-    the value times the desired percent error. Returns the error.
-    """
-    sigma = percent_error * value
-    error = np.random.normal(loc=value, scale=sigma, size=None)
-    return(error)
+def redden(redshift, spectrum):
+    spectrum[:,0] *= (1.0 + redshift)
+    return(spectrum)
 
-def noisify_2(values, percent_error):
-    """
-    Receives an array of values and desired percent error. For each value,
-    draws a number from a Gaussian distribution around that value, with
-    a width equal to twice the median of the data times the percent error.
+def deredden(redshift, spectrum):
+    spectrum[:,0] /= (1.0 + redshift)
+    return(spectrum)
+
+def noisify(values, percent_error):
+    """
+    Receives an array of values and desired percent error. For each value,
+    draws a number from a Gaussian distribution around that value, with
+    a width equal to twice the median of the data times the percent error.
     Returns an array of these errors.
-    """
-    sigma = percent_error * np.median(values)
-    errors = np.random.normal(loc=values, scale=sigma, size=values.shape)
-    return(errors)
+    """
+    sigma = percent_error * np.median(values)
+    errors = np.abs(np.random.normal(loc=0.0, scale=sigma, size=(values.shape[0],1)))
+    return(errors)
 
 def export_spectrum(filename, model, percent_error):
     """
@@ -35,7 +33,7 @@ def export_spectrum(filename, model, percent_error):
     for key, value in model.model_comp.items():
         try: # The value is another dictionary.
             for subkey, subvalue in value.items():
-                header += subkey + ":" + str(subvalue) + "\n"
+                header += key + ":" + subkey + ":" + str(subvalue) + "\n"
         except AttributeError: # The value is not another dictionary.
             header += key + ":" + str(value) + "\n"
     # Strip the last newline from the header.
@@ -44,25 +42,19 @@ def export_spectrum(filename, model, percent_error):
     # Extract the wavelength and flux value of full spectrum from the object.
     spectrum = model.spectrum
     # Generate noisy errors for the spectrum.
-    errors = noisify_1(spectrum[:,1], percent_error)
+    errors = noisify(spectrum[:,1], percent_error)
     # Combine the wavelengths, fluxes, and flux errors into one array.
-    spectrum_with_errs = np.concatenate(spectrum, errors, axis=1)
+    spectrum_with_errs = np.append(spectrum, errors, axis=1)
     # Write the data with "secret" model components header to a file.
     if filename[-4:] != ".csv": filename += ".csv"
-    np.savetxt("data/"+filename, spectrum_with_errs, delimiter=", ", header=header)
-
-def export_sfh(filename, model):
-    pass
+    np.savetxt("data/"+filename, spectrum_with_errs, delimiter=" ", header=header)
 
 def load_data(filename):
     """
     Data import function for the __init__() method of bagpipes.galaxy.
     """
     # Load the data and extract into wavelength and flux arrays.
-    spectrum = np.loadtxt("data/" + filename, delimiter=", ")
-    spectrum_with_errs = np.zeros((spectrum.shape[0], 3))
-    spectrum_with_errs[:,0:2] = np.copy(spectrum)
-    spectrum_with_errs[:,2] = np.copy(np.sqrt(spectrum[:,1]))
+    spectrum_with_errs = np.loadtxt("data/" + filename, delimiter=" ")
     return(spectrum_with_errs)
 
 def load_xshooter(ID):
@@ -70,23 +62,8 @@ def load_xshooter(ID):
     Data import function for the __init__ method of bagpipes.galaxy. Loads in
     wavelengths and fluxes from the  XSHOOTER .asci files.
     """
-    spectrum = np.loadtxt("data/20200127_xshoot_corr.asci", dtype="float")
-    spectrum_with_errs = np.zeros((spectrum.shape[0], 3))
-    spectrum_with_errs[:,0:2] = np.copy(spectrum)
-    spectrum_with_errs[:,2] = np.copy(np.sqrt(spectrum[:,1]))
-    print("This is legacy xshooter!")
-    return(spectrum_with_errs)
-
-def load_xshooter_2(ID):
-    """
-    Data import function for the __init__ method of bagpipes.galaxy. Loads in
-    wavelengths and fluxes from the  XSHOOTER .asci files.
-    """
     spectrum_with_errs = np.loadtxt("data/"+ID+".txt", dtype="float")
-    # spectrum_with_errs = np.zeros((spectrum.shape[0], 3))
-    # spectrum_with_errs[:,0:2] = np.copy(spectrum)
-    # spectrum_with_errs[:,2] = np.copy(np.sqrt(spectrum[:,1]))
-    print("This is the new xshooter!")
+    spectrum_with_errs = deredden(0.046, spectrum_with_errs)
     return(spectrum_with_errs)
 
 def import_spectrum(filename):
@@ -126,6 +103,9 @@ def import_spectrum(filename):
     galaxy = pipes.galaxy(filename, load_data, photometry_exists=False)
     # Return the newly created object and the reconstructed dictionary.
     return(galaxy, model_components)
+
+def export_sfh(filename, model):
+    pass
 
 def import_sfh(filename):
     pass
