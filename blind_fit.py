4832b1f554aaa7a1d6be72100465431c6d3400fb
aswheaton <alex.s.wheaton@gmail.com>
2021-02-03 15:01:15 +0000

    Added progress indicators, fixed key errors for blind fitting algorithm.

--- blind_fit.py
+++ blind_fit.py
@@ -30,11 +30,11 @@ delayed["tau"] = (0.0, 2.0)           # Timescale of decrease: Gyr
 delayed["massformed"] = (0.0, 15.0)
 delayed["metallicity"] = (0.0, 2.5)
 
-lognormal1 = {}
-lognormal1["tmax"] = (3.5, 10.5)
-lognormal1["fwhm"] = (0.1, 5.0)
-lognormal1["massformed"] = (0.0, 15.0)
-lognormal1["metallicity"] = (0.0, 2.5)
+lognormal = {}
+lognormal["tmax"] = (3.5, 10.5)
+lognormal["fwhm"] = (0.1, 5.0)
+lognormal["massformed"] = (0.0, 15.0)
+lognormal["metallicity"] = (0.0, 2.5)
 
 dust = {}
 dust["type"] = "Calzetti"
@@ -51,41 +51,48 @@ fit_instructions = {
 for filename in datafiles:
 
     # Do an initial fit with only an exponential compontent, over a large parameter space.
+    print("Running initial exponential fit for {}...".format(filename)),
     galaxy, model_components = import_spectrum(filename)
     fit = pipes.fit(galaxy, fit_instructions, run="exp_noburst")
-    fit.fit(verbose=True)
+    fit.fit(verbose=False)
+    print("Done!")
 
     # Now run a new fit with other functional components with age constrained to one standard deviation around the exponential age.
     age_lower_bound = np.percentile(fit_instructions["exponential"]["age"], 16)
     age_upper_bound = np.percentile(fit_instructions["exponential"]["age"], 84)
 
     # Create a dictionary for storying posterior sample distribution widths.
-    sigma_values = {"exponential" : np.percentile(fit.posterior.samples["age"], 84) - np.percentile(fit.posterior.samples["age"], 16)}
+    sigma_values = {"exponential" : np.percentile(fit.posterior.samples["exponential:age"], 84) - np.percentile(fit.posterior.samples["exponential:age"], 16)}
 
     fit_instructions.pop("exponential", None)
     # dblplaw["age"] = ()
     # fit_instructions["dblplaw"] = dblplaw
     # fit = pipes.fit(galaxy, fit_instructions, run="dblplaw_noburst")
-    # sigma_values["dblplaw"] = np.percentile(fit.posterior.samples["age"], 84) - np.percentile(fit.posterior.samples["age"], 16)
     # fit.fit(verbose=True)
+    # sigma_values["dblplaw"] = np.percentile(fit.posterior.samples["age"], 84) - np.percentile(fit.posterior.samples["age"], 16)
     #
+    print("Running delayed fit for {}...".format(filename)),
     # fit_instructions.pop("dblplaw", None)
     delayed["age"] = (age_lower_bound, age_upper_bound)
     fit_instructions["delayed"] = delayed
     fit = pipes.fit(galaxy, fit_instructions, run="delayed_noburst")
-    sigma_values["delayed"] = np.percentile(fit.posterior.samples["age"], 84) - np.percentile(fit.posterior.samples["age"], 16)
-    fit.fit(verbose=True)
+    fit.fit(verbose=False)
+    sigma_values["delayed"] = np.percentile(fit.posterior.samples["delayed:age"], 84) - np.percentile(fit.posterior.samples["delayed:age"], 16)
+    print("Done!")
 
+    print("Running lognormal fit for {}...".format(filename)),
     fit_instructions.pop("delayed", None)
     lognormal["tmax"] = (13.5 - age_upper_bound, 13.5 - age_lower_bound)
     fit_instructions["lognormal"] = lognormal
     fit = pipes.fit(galaxy, fit_instructions, run="lognormal_noburst")
-    sigma_values["lognormal"] = np.percentile(fit.posterior.samples["tmax"], 84) - np.percentile(fit.posterior.samples["tmax"], 16)
-    fit.fit(verbose=True)
+    fit.fit(verbose=False)
+    sigma_values["lognormal"] = np.percentile(fit.posterior.samples["lognormal:tmax"], 84) - np.percentile(fit.posterior.samples["lognormal:tmax"], 16)
+    print("Done!")
 
     # Get the functional form with the "tightest" contraints on age.
     best_func = min(sigma_values, key=lambda k: sigma_values[k])
 
+    print("Running a {} and burst fit for {}...".format(best_func, filename)),
     # Now run a fit with that functional form AND a burst in recent history.
     fit_instructions.pop("lognormal", None)
     fit_instructions[best_func] = eval(best_func)
@@ -97,7 +104,8 @@ for filename in datafiles:
 
     fit_instructions["burst"] = burst
     fit = pipes.fit(galaxy, fit_instructions, run=best_func+"_burst")
-    fit.fit(verbose=True)
+    fit.fit(verbose=False)
+    print("Done!")
 
 
 print ('parameter     median     16th percentile     84th percentile')
