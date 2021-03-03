d4fb0bb26cda3218ef80a06e2be17d840d615526
aswheaton <alex.s.wheaton@gmail.com>
2021-03-03 12:05:34 +0000

    Test files for using Slurm on the Cuillin computing cluster.

---
+++ slurmtest.py
@@ -0,0 +1,4 @@
+# Tests the import of the bagpipes module.
+import bagpipes as pipes
+# This prints something to STDOUT for nohup to record.
+print("This is test output to a file in the nohup directory.")
