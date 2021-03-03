fb412b50aba746f2660e251b4b4aabec4331fc2a
aswheaton <alex.s.wheaton@gmail.com>
2021-03-03 18:22:02 +0000

    Reconfigured scripts for running on Cuillin.

--- slurmtest.py
+++
@@ -1,4 +0,0 @@
-# Tests the import of the bagpipes module.
-import bagpipes as pipes
-# This prints something to STDOUT for nohup to record.
-print("This is test output to a file in the nohup directory.")
